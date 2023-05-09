6a082c3bda887c0554f4c051598b2674a16d118d
iamhung <43690592+lequochung19971@users.noreply.github.com>
2023-05-09 11:26:53 +0700

    feat(web): update text

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Button } from "ui";
+import { Button } from 'ui';
 
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1>Web 23</h1>
       <Button />
     </div>
   );
